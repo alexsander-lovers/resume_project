9062027c02a93c5766d66126303b77b3a08c6edd
alexsander-lovers <150514172+alexsander-lovers@users.noreply.github.com>
2026-08-22 16:29:33 +0300

    Updates

--- source/js/app.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.navigation');
-
-burger.addEventListener('click', () => {
-  menu.classList.toggle('navigation--is-open');
-  burger.classList.toggle('burger--is-active');
-});
