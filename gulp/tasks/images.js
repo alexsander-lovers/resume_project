b32a865b3923b55725fe88a326683e7a8ce38953
alexsander-lovers <150514172+alexsander-lovers@users.noreply.github.com>
2026-08-27 18:41:08 +0300

    Дорабатывает блоки чистит стили

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,9 +2,8 @@ import webp from 'gulp-webp';
 import avif from 'gulp-avif';
 import imagemin from 'gulp-imagemin';
 
-// 1. Задача для ковертации (Avif)
 export const convertToAvif = () => {
-  return app.gulp.src(app.path.source.images, { encoding: false }) // Убедитесь, что тут исключены SVG (например, через маску *.{jpg,jpeg,png})
+  return app.gulp.src(app.path.source.images, { encoding: false })
     .pipe(app.plugins.plumber(
       app.plugins.notify.onError({
         title: 'IMAGES: AVIF',
@@ -12,13 +11,13 @@ export const convertToAvif = () => {
       })
     ))
     .pipe(app.plugins.newer(app.path.build.images))
-    .pipe(app.plugins.if(app, avif({ quality: 80 }))) // Явно задаем качество, чтобы избежать багов сжатия по умолчанию
+    .pipe(app.plugins.if(app, avif({ quality: 80 })))
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.plugins.browsersync.stream());
 };
 // 2. Задача для ковертации (Webp)
 export const convertToWebp = () => {
-  return app.gulp.src(app.path.source.images, { encoding: false }) // Убедитесь, что тут исключены SVG (например, через маску *.{jpg,jpeg,png})
+  return app.gulp.src(app.path.source.images, { encoding: false })
     .pipe(app.plugins.plumber(
       app.plugins.notify.onError({
         title: 'IMAGES: WEBP',
@@ -26,11 +25,11 @@ export const convertToWebp = () => {
       })
     ))
     .pipe(app.plugins.newer(app.path.build.images))
-    .pipe(app.plugins.if(app, webp({ quality: 80 }))) // Явно задаем качество, чтобы избежать багов сжатия по умолчанию
+    .pipe(app.plugins.if(app, webp({ quality: 80 })))
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.plugins.browsersync.stream());
 };
-// 3. Задача для оптимизации оригиналов (JPG, PNG)
+
 export const optimizeRaster = () => {
   return app.gulp.src(app.path.source.images, { encoding: false })
     .pipe(app.plugins.plumber(
@@ -48,7 +47,7 @@ export const optimizeRaster = () => {
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.plugins.browsersync.stream());
 };
-// // 4. Задача для SVG
+
 export const copySvg = () => {
   return app.gulp.src(app.path.source.svg)
     .pipe(app.gulp.dest(app.path.build.images))
--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,24 @@ burger.addEventListener('click', () => {
   menu.classList.toggle('navigation--is-open');
   burger.classList.toggle('burger--is-active');
 });
+
+
+const buttons = document.querySelectorAll('.button-js');
+const closeBtn = document.querySelector('.button-close');
+const modal = document.querySelector('.modal');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
